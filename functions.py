361b4fd49009b00d2f1d420d4bc51db6e57aaa95
shanunath29 <158049576+shanunath29@users.noreply.github.com>
2024-02-09 16:21:48 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,105 @@
+import time
+# print("We are united")
+
+# for p in range(10) :
+#     print("We are united")
+#range(10) =>[0,10) =>0,1,2,3,4,5,6,7,8,9
+    
+# array
+# array is a data structure similar type of data can be stored
+    
+# int arr[10];
+# arr =[]
+# arr =["apple","lemon","orange","dragon fruit"]
+# print(arr[0])
+# print(arr[1])
+# print(arr[2])
+# print("---------")
+
+# for fruit in [0,1,2,3,4,5,6,7,8,9] :
+#     print(fruit)
+
+# range(10) =>[0,1,2,3,4,5,6,7,8,9]
+
+# fruit ="banana"
+# for f in fruit : #[b,a,n,a,n,a]
+#     print(f)
+
+
+# range(10) =>[0,1,2,3,4,5,6,7,8,9]
+# for i in range(2,10): #[3,4,5,6,7,8,9] =>(2,10)
+#     print(i)
+
+# for i in range(2,10,2): #[2,_,4,_,6,_,8] =>(2,10)
+#     print(i)
+
+# while condition :
+#     expression
+
+# v =6
+# print("loop start")
+# while v>5 :
+#     print(v)
+#     # time.sleep(1)
+#     v=v+1
+
+# print("loop end")
+
+# sum=1+2+...100
+# sum=0
+# start=1
+# while start<=100 :
+#     sum=sum+start
+#     start=start +1
+
+# print(sum)
+
+# if else
+
+# score = float(input("Enter your score :"))
+
+# print("your score is :",score)
+
+# if score > 80 : 
+#     print("grade is A+")
+# elif score > 60 and  score<80: 
+#     print("grade is A")
+# elif score > 40 and  score<60 :
+#     print("grade is B")
+# else :
+#     print("grade is F")
+
+# if score>80:
+#      print("grade is A+")
+# elif
+
+# a=10
+# b=40
+# if a>b :
+#     print(a)
+# else:
+#     print(b)
+# x=2
+# print("loop start")
+# for x in range(4) :
+#     print("----")
+#     print(x)
+#     print(x)
+#     print("with in loop")
+#     print(x)
+#     print("----")
+
+
+# print("loop ended")
+
+
+# print(x)
+
+# while condition : a>b/b<a a!-b
+    # expression =>print() => sum=sum+1
+
+# i=0
+# b=12
+# while i<b :
+#     print(i)
+#     i=i+1
